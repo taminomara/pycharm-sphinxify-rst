35d12dc1a3153e50a3934e0dab0fe39afd8319f6
GitHub Action <action@github.com>
2023-04-06 16:38:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/taminomara/pycharmsphinxifyrst/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/taminomara/pycharmsphinxifyrst/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.taminomara.pycharmsphinxifyrst.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.taminomara.pycharmsphinxifyrst.MyBundle
+import com.github.taminomara.pycharmsphinxifyrst.services.MyProjectService
 import javax.swing.JButton
 
 
